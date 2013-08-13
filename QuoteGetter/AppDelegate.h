b60c7637a105746f51b2e203a7a5b18a6c4ac386
privat <privat@rolf-paspirgilis-computer.fritz.box>
2013-08-13 19:57:53 +0200

    Initial commit

---
+++ QuoteGetter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuoteGetter
+//
+//  Created by privat on 13.08.13.
+//  Copyright (c) 2013 rpaspi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuoteGetter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuoteGetter
+//
+//  Created by privat on 13.08.13.
+//  Copyright (c) 2013 rpaspi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
