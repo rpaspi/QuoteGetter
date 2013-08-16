5bf75ca27d384db3b62f7d25d6c65940ff3fefb5
privat <privat@rolf-paspirgilis-computer.local>
2013-08-16 15:11:35 +0200

    Quotes-Classes added

---
+++ QuoteGetter/Quotes.h
@@ -0,0 +1,27 @@
+//
+//  Quotes.h
+//  CommTest
+//
+//  Created by privat on 09.07.13.
+//  Copyright (c) 2013 rpaspi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Quotes : NSObject <NSXMLParserDelegate>
+
+@property (nonatomic, strong) NSString *symbol;
+@property (nonatomic, strong) NSNumber *last;
+@property (nonatomic, strong) NSNumber *change;
+@property (nonatomic, strong) NSNumber *percentageChange;
+@property (nonatomic, strong) NSString *currentTag;
+@property (nonatomic, strong) NSString *oldTag;
+@property (nonatomic, strong) NSXMLParser *parser;
+
+- (void) update;
+- (void) initWithSymbol:(NSString*) quoteSymbol;
+
+// Parser Delegate Methods
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict;
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string;
+@end
---
+++ QuoteGetter/Quotes.m
@@ -0,0 +1,82 @@
+//
+//  Quotes.m
+//  CommTest
+//
+//  Created by privat on 09.07.13.
+//  Copyright (c) 2013 rpaspi. All rights reserved.
+//
+
+#import "Quotes.h"
+#import "StockQuoteSvc.h"
+
+@implementation Quotes
+
+@synthesize symbol = _symbol;
+@synthesize last = _last;
+@synthesize change = _change;
+@synthesize percentageChange = _percentageChange;
+@synthesize currentTag = _currentTag;
+@synthesize oldTag = _oldTag;
+@synthesize parser = _parser;
+
+- (void) initWithSymbol:(NSString*) quoteSymbol {
+    self.currentTag = @"";
+    self.oldTag = @"";
+    self.last = @(0);
+    self.change = @(0);
+    self.percentageChange = @(0);
+    self.symbol = [NSString stringWithString:quoteSymbol];
+    [self update];
+}
+
+- (void) update {
+    StockQuoteSoap12Binding *binding = [[StockQuoteSoap12Binding alloc] initWithAddress:@"http://www.webservicex.net/stockquote.asmx?WSDL"];
+    StockQuoteSvc_GetQuote *parameters = [[StockQuoteSvc_GetQuote alloc] init];
+    parameters.symbol = self.symbol;
+    StockQuoteSoap12BindingResponse *response = [[StockQuoteSoap12BindingResponse alloc] init];
+    //StockQuoteSvc_GetQuoteResponse
+    response = [binding GetQuoteUsingParameters:parameters];
+    NSArray *ergebnisse = [NSArray arrayWithArray:response.bodyParts];
+    if ([ergebnisse count] > 0) {
+        NSString *quoteResult = [NSString stringWithString:((StockQuoteSvc_GetQuoteResponse*)ergebnisse[0]).GetQuoteResult];
+        if (!_parser) {
+            self.parser =[[NSXMLParser alloc] initWithData:[quoteResult dataUsingEncoding:NSUTF8StringEncoding]];
+            [self.parser setDelegate:self];
+        } else {
+            [self.parser abortParsing];
+            self.parser = [self.parser initWithData:[quoteResult dataUsingEncoding:NSUTF8StringEncoding]];
+        }
+        [self.parser parse];
+    }
+}
+
+// Parser Delegate Methods
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict {
+    if ([elementName isEqualToString:@"Last"] || [elementName isEqualToString:@"Change"] || [elementName isEqualToString:@"PercentageChange"]) {
+        self.currentTag = elementName;
+    }
+}
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
+    if (![self.oldTag isEqualToString:self.currentTag]) {
+        if ([self.currentTag isEqualToString:@"Last"]) {
+            self.last = @([string floatValue]);
+        } else if ([self.currentTag isEqualToString:@"Change"]) {
+            self.change = @([string floatValue]);
+        } else if ([self.currentTag isEqualToString:@"PercentageChange"]) {
+            self.percentageChange = @([string floatValue]);
+        }
+        self.oldTag = self.currentTag;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+@end
